441a0e7e27490067440cad9aab793d7e51a63db7
pseudoankit <lostankit7@gmail.com>
2022-12-18 18:45:43 +0530

    create utility to cache list of days in a month
    Signed-off-by: pseudoankit <lostankit7@gmail.com>

---
+++ core/core/src/main/java/pseudoankit/droid/core/util/datetime/DateUtils.kt
@@ -0,0 +1,27 @@
+package pseudoankit.droid.core.util.datetime
+
+import java.time.LocalDate
+import java.time.Month
+
+object DateUtils {
+    private val dayRangeOfMonth = mutableMapOf<Month, List<LocalDate>>()
+
+    /**
+     * @return[List<LocalDate>] list of days which falls in the same month passed in the param
+     * @param[date] date for which range needed
+     */
+    fun getDayRangeForDate(date: LocalDate): List<LocalDate> {
+        return dayRangeOfMonth.getOrPut(date.month) {
+            (1..date.lengthOfMonth()).map {
+                LocalDate.of(date.year, date.month, it)
+            }
+        }.map {
+            // returns the same date object if year is same to the date passed by client
+            // else mapping the day range to update the year
+            if (it.year == date.year) {
+                return@map it
+            }
+            LocalDate.of(date.year, it.month, it.dayOfMonth)
+        }
+    }
+}
