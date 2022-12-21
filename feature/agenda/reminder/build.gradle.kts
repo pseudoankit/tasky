7532015f4b3a858984aa29de86358511b9a23f1a
pseudoankit <lostankit7@gmail.com>
2022-12-21 07:55:01 +0530

    added compose destination config
    Signed-off-by: pseudoankit <lostankit7@gmail.com>

--- feature/agenda/reminder/build.gradle.kts
+++ feature/agenda/reminder/build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
     id(Plugins.Library.Compose.Feature)
 }
 
+ksp {
+    arg("compose-destinations.mode", "destinations")
+    arg("compose-destinations.moduleName", "agenda-reminder")
+}
+
 android {
     namespace = "pseudoankit.droid.tasky.reminder"
 }
--- feature/agenda/task/build.gradle.kts
+++ feature/agenda/task/build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
     id(Plugins.Library.Compose.Feature)
 }
 
+ksp {
+    arg("compose-destinations.mode", "destinations")
+    arg("compose-destinations.moduleName", "agenda-task")
+}
+
 android {
     namespace = "pseudoankit.droid.tasky.task"
 }
