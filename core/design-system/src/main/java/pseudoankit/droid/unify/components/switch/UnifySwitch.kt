1a18bb614b7d9dac2b44b097088740122e677e7c
pseudoankit <lostankit7@gmail.com>
2022-12-31 16:35:14 +0530

    UnifySwitch implementation
    Signed-off-by: pseudoankit <lostankit7@gmail.com>

--- core/design-system/src/main/java/pseudoankit/droid/unify/components/switch/UnifySwitch.kt
+++ core/design-system/src/main/java/pseudoankit/droid/unify/components/switch/UnifySwitch.kt
@@ -1,8 +1,14 @@
 package pseudoankit.droid.unify.components.switch
 
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import pseudoankit.droid.unify.token.UnifyColors
 
 object UnifySwitch {
 
@@ -14,7 +20,7 @@ object UnifySwitch {
                 onCheckedChange?.invoke()
             },
             enabled = onCheckedChange != null,
-            colors = UnifySwitchInternal.switchColors
+            colors = switchColors
         )
     }
 
@@ -23,4 +29,40 @@ object UnifySwitch {
         val onCheckedChange: (() -> Unit)?,
         val modifier: Modifier = Modifier,
     )
+
+    private val switchColors
+        @Composable get() = object : SwitchColors {
+            val defaultColors = SwitchDefaults.colors()
+
+            @Composable
+            override fun borderColor(enabled: Boolean, checked: Boolean): State<Color> {
+                return mutableStateOf(UnifyColors.Transparent)
+            }
+
+            @Composable
+            override fun iconColor(enabled: Boolean, checked: Boolean): State<Color> {
+                return defaultColors.iconColor(enabled = enabled, checked = checked)
+            }
+
+            @Composable
+            override fun thumbColor(enabled: Boolean, checked: Boolean): State<Color> {
+                return mutableStateOf(
+                    when (enabled) {
+                        true -> UnifyColors.Black
+                        false -> UnifyColors.Gray100
+                    }
+                )
+            }
+
+            @Composable
+            override fun trackColor(enabled: Boolean, checked: Boolean): State<Color> {
+                return mutableStateOf(
+                    when (enabled) {
+                        true -> UnifyColors.Black.copy(alpha = .1f)
+                        false -> UnifyColors.Gray100.copy(alpha = .42f)
+                    }
+                )
+            }
+
+        }
 }
