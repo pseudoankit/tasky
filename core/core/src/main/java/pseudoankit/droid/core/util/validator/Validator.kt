50d1dbecc781c26f33f1059c0f16fe3b28a153af
pseudoankit <lostankit7@gmail.com>
2023-01-07 22:34:09 +0530

    validator updated
    Signed-off-by: pseudoankit <lostankit7@gmail.com>

--- core/core/src/main/java/pseudoankit/droid/core/util/validator/Validator.kt
+++ core/core/src/main/java/pseudoankit/droid/core/util/validator/Validator.kt
@@ -3,27 +3,31 @@ package pseudoankit.droid.core.util.validator
 import pseudoankit.droid.core.util.TextResource
 
 object Validator {
+    const val EMAIL_PATTERN = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}\$"
 
-    fun validateEmail(
-        email: String?,
-        emptyErrorMessage: TextResource = TextResource.NormalString("Email cannot be empty")
-    ): TextResource? {
-        if (email.isNullOrBlank()) return emptyErrorMessage
+    /**
+     * email validator returns null if email is valid else will return error message
+     */
+    fun validateEmail(email: String?): TextResource? {
+        if (email.isNullOrBlank()) return TextResource.NormalString("Email cannot be empty")
 
-        // todo match pattern
+        val isValid = email.matches(Regex(pattern = EMAIL_PATTERN))
+        if (isValid.not()) return TextResource.NormalString("You have entered an invalid email address")
 
         return null
     }
 
+    /**
+     * password validator returns null if email is valid else will return error message
+     */
     fun validatePassword(
         password: String?,
         emptyErrorMessage: TextResource = TextResource.NormalString("Password cannot be empty"),
-        invalidLengthErrorMessage: TextResource = TextResource.NormalString("Password cannot be of less than 6 digits"),
+        invalidLengthErrorMessage: TextResource = TextResource.NormalString("Password cannot be less than 6 digits"),
     ): TextResource? {
         if (password.isNullOrBlank()) return emptyErrorMessage
+        if (password.contains(' ')) return TextResource.NormalString("Password cannot contain whitespaces")
         if (password.length < 6) return invalidLengthErrorMessage
-
-        // todo match pattern
 
         return null
     }
