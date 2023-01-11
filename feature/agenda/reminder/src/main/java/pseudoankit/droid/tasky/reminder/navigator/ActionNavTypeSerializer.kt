5e61943a7b9dab3cad2d7503f7e2754f5e113c61
pseudoankit <lostankit7@gmail.com>
2023-01-11 23:32:22 +0530

    route serializer added for agenda action
    Signed-off-by: pseudoankit <lostankit7@gmail.com>

---
+++ feature/agenda/reminder/src/main/java/pseudoankit/droid/tasky/reminder/navigator/ActionNavTypeSerializer.kt
@@ -0,0 +1,26 @@
+package pseudoankit.droid.tasky.reminder.navigator
+
+import com.ramcosta.composedestinations.navargs.DestinationsNavTypeSerializer
+import com.ramcosta.composedestinations.navargs.NavTypeSerializer
+import pseudoankit.droid.agendamanger.domain.model.AgendaTypes
+
+@NavTypeSerializer
+class ActionNavTypeSerializer : DestinationsNavTypeSerializer<AgendaTypes.Action> {
+    private companion object {
+        const val CREATE = 0L
+    }
+
+    override fun toRouteString(value: AgendaTypes.Action): String {
+        return when (value) {
+            AgendaTypes.Action.Create -> "$CREATE"
+            is AgendaTypes.Action.Edit -> "${value.id}"
+        }
+    }
+
+    override fun fromRouteString(routeStr: String): AgendaTypes.Action {
+        return when (val id = routeStr.toLongOrNull()) {
+            null, CREATE -> AgendaTypes.Action.Create
+            else -> AgendaTypes.Action.Edit(id)
+        }
+    }
+}
