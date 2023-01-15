992cb2886d88f8ce7fb0a18331af3a8760be31a0
pseudoankit <lostankit7@gmail.com>
2023-01-15 20:28:30 +0530

    Merge remote-tracking branch 'origin/master' into image
    Signed-off-by: pseudoankit <lostankit7@gmail.com>
    
    # Conflicts:
    #	core/design-system/build.gradle.kts

--- core/design-system/src/debug/java/pseudoankit/droid/unify/UnifyActivity.kt
+++ core/design-system/src/debug/java/pseudoankit/droid/unify/UnifyActivity.kt
@@ -8,10 +8,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Divider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import pseudoankit.droid.unify.componentsdemo.UnifyImageDemo
 
 fun Context.navigateToUnifyDemo() {
     Intent(this, UnifyActivity::class.java).apply {
@@ -30,8 +28,6 @@ class UnifyActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 item {
-                    UnifyImageDemo()
-                    Divider()
                 }
             }
         }
