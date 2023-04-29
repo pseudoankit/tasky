66174eaad8c36298e270b92715f4e89b6e2a2b0b
pseudoankit <lostankit7@gmail.com>
2023-04-29 12:48:45 +0530

    updated start route as per login status

--- core/navigation/src/main/java/pseudoankit/droid/navigation/navgraph/MainNavGraph.kt
+++ core/navigation/src/main/java/pseudoankit/droid/navigation/navgraph/MainNavGraph.kt
@@ -4,14 +4,16 @@ import com.ramcosta.composedestinations.spec.DestinationSpec
 import com.ramcosta.composedestinations.spec.NavGraphSpec
 import com.ramcosta.composedestinations.spec.Route
 import pseudoankit.droid.authentication.presentation.authenticationDestinations
+import pseudoankit.droid.authentication.presentation.destinations.LoginScreenDestination
 import pseudoankit.droid.tasky.home.presentation.destinations.HomeScreenDestination
 import pseudoankit.droid.tasky.home.presentation.homeDestinations
 import pseudoankit.droid.tasky.reminder.presentation.ui.destinations.ReminderScreenDestination
 
-object MainNavGraph : NavGraphSpec {
+fun mainNavGraph(isUserLoggedIn: Boolean) = object : NavGraphSpec {
     override val route: String = "Tasky"
 
-    override val startRoute: Route = HomeScreenDestination
+    override val startRoute: Route =
+        if (isUserLoggedIn) HomeScreenDestination else LoginScreenDestination
 
     override val destinationsByRoute: Map<String, DestinationSpec<*>>
         get() = destinations.associateBy {
