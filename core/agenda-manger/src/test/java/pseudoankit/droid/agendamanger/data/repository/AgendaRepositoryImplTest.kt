2ea420b86908165bcd51374667ad9e4eec6703b9
ankitkumar <lostankit7@gmail.com>
2023-12-31 22:49:28 +0530

    added tests

--- core/agenda-manger/src/test/java/pseudoankit/droid/agendamanger/data/repository/AgendaRepositoryImplTest.kt
+++ core/agenda-manger/src/test/java/pseudoankit/droid/agendamanger/data/repository/AgendaRepositoryImplTest.kt
@@ -1,7 +1,28 @@
 package pseudoankit.droid.agendamanger.data.repository
 
+import io.mockk.coEvery
+import io.mockk.mockk
+import org.junit.Test
+import pseudoankit.droid.agendamanger.domain.model.AgendaItem
+import pseudoankit.droid.agendamanger.domain.repository.ReminderRepository
+import kotlin.test.assertEquals
+
 
 internal class AgendaRepositoryImplTest {
 
+    private val reminderRepository: ReminderRepository = mockk()
+    private val repository = AgendaRepositoryImpl(reminderRepository)
 
+
+    @Test
+    fun `getAllSavedItem should return data from reminderRepository`() {
+        val expected = listOf(
+            AgendaItem.Reminder()
+        )
+        coEvery { reminderRepository.getReminders() } returns expected
+
+        val actual = repository.getAllSavedItem()
+
+        assertEquals(expected, actual)
+    }
 }
