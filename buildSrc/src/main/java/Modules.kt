1e0da418ccc4f0792208f05fc1a4e1533fe56af6
ankitkumar <lostankit7@gmail.com>
2024-02-02 02:40:10 +0530

    generated and used type safe projects

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -1,15 +1,4 @@
 object Modules {
-
-    object Feature {
-        const val Authentication = ":feature:authentication"
-        const val Home = ":feature:home"
-        const val Event = ":feature:agenda:event"
-        const val Reminder = ":feature:agenda:reminder"
-        const val Task = ":feature:agenda:task"
-        const val Splash = ":feature:splash"
-        const val DeveloperTools = ":feature:developer-tools"
-    }
-
     object Core {
         const val Core = ":core:core"
         const val CoreUi = ":core:core-ui"
